afe5ef5e6f6cb20e7f9b7780a5cf168d170bbe25
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-02-16 19:38:49 +0300

    start

---
+++ task_3/main.py
@@ -0,0 +1,66 @@
+import json
+
+
+class Parse():
+    response = {}
+
+    def __init__(self, file1, file2) -> None:
+        self.file1 = file1 
+        self.file2 = file2
+
+    def open_json(self, path):
+        with open(path, 'rb') as file:
+            print(file)
+            result = json.load(file)
+        return result
+
+
+    def run_sript(self):
+        json_value = self.open_json(self.file1)
+        json_test = self.open_json(self.file2)
+        value = json.loads(json_value)
+        test = json.loads(json_test)
+        self.parse_json(value, False)
+        self.parse_json(test, True)
+        report_json = json.dumps(test, indent=4)
+        print(report_json)
+        return report_json
+
+    def parse_json(self, test, write=False):
+        if isinstance(test, dict):
+            for item in test:
+                if isinstance(test.get(item), list):
+                    for x in test.get(item):
+                        if not write:
+                            self.change_value(x)
+                            if x.get('values'):
+                                self.parse_json(x['values'], write)
+                        elif write:
+                            self.write_json(x)
+                            if x.get('values'):
+                                self.parse_json(x['values'], write)
+        elif isinstance(test, list):
+            for i in test:
+                if not write:
+                    self.change_value(i)
+                elif write:
+                    self.write_json(i)
+                self.parse_json(i, write)
+
+    def change_value(self, dct):
+        id = dct.get('id')
+        value = dct.get('value')
+        self.response[id] = value
+
+    def write_json(self, dct):
+        dct['value'] = self.response.get(dct['id'])
+
+
+def task3(file1, file2):
+    task = Parse(file1, file2)
+    return task.run_sript()
+
+
+path_value = '/home/vjachesalv/Performance_lab/task_3/fixture/values.json'
+path_test = '/home/vjachesalv/Performance_lab/task_3/fixture/tests.json'
+task3(path_value, path_test)
